fe249083ccd75895d5866113e8d6d0a597387618
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-04-04 03:46:28 +0000

    updated theme selector

--- classdemo/src/index.js
+++ classdemo/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 // import './sass/App.scss';
 import App from './App';
@@ -7,11 +7,13 @@ const LightTheme = React.lazy(() => import('./components/LightTheme'));
 const DarkTheme = React.lazy(() => import('./components/DarkTheme'));
 
 const ThemeSelector = ({children}) => {
-  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+  const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : window.matchMedia('(prefers-color-scheme: dark)').matches
+  localStorage.setItem('theme', theme)
   return(
-    <React.Suspense fallback={<></>}>
+    <React.Suspense fallback={<>...Loading</>} key={theme}>
       {theme === 'dark' && <DarkTheme />}
       {theme === 'light' && <LightTheme />}
+      <button onClick={() => {localStorage.setItem('theme', localStorage.getItem('theme') === 'dark' ? 'light' : 'dark'); window.location.reload()}}>Change Mode</button>
       {children}
     </React.Suspense>
   )
