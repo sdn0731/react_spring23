cca71304a383b1eb344498148ebfb9323e2e4c67
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-04-04 17:38:39 +0000

    added testing

---
+++ classdemo/src/tests/Header.test.js
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+test('renders header', () => {
+    Object.defineProperty(window, 'matchMedia', {
+        value: () => {
+            return {
+                matches: false,
+                addEventListener: () => {},
+                removeEventListener: () => {}
+            }
+        }
+    })
+    render(<Header linkText="search me" linkUrl="www.google.com" />);
+    const linkElement = screen.getByText(/and save to reload/i);
+    const count = screen.getByText(/You have clicked on the button/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(count).toBeInTheDocument();
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+});
+
+/* Alt Props Test
+const props = { linkText:"search me", linkUrl:"www.google.com"}
+render(<Header {...props}/>);
+*/
